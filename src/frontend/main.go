621f885c7eee6c75eba6d2d16b6a21b29cd48bbc
darshand241168 <131633694+darshand241168@users.noreply.github.com>
2023-05-26 15:20:25 +1000

    Update main.go to include comment about Darshan

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This is a forked copy of the demo that is under the control of Darshan
+// Dillon.
+
 package main
 
 import (
